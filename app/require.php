632dbdebde970109dff6621c0117370c0b134d98
MRavindu <68803688+MRavindu@users.noreply.github.com>
2021-11-16 15:43:11 +0530

    Defined Paths of require files

---
+++ app/require.php
@@ -0,0 +1,5 @@
+<?php
+    //Require libraries from the folder libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
